1130836b6c1139b80d1e89c517b1d188d6ff2dce
mrkalilano <202280131@psu.palawan.edu.ph>
2024-11-15 17:30:24 +0800

    configuring database and installing mysql.connector

---
+++ another_version.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from http import HTTPStatus
+import mysql.connector
+
+app = Flask(__name__)
+
+# Database configuration
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': 'root',
+    'database': 'library'
+}
+
